21bdf1c8cf625a960a44c73319fb62ae9fcc5067
amabbymrtnz <amabigailmrtnz@gmail.com>
2023-10-22 18:12:47 -0600

    syntax changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Meal Manager lets you add your meals for the week so you can remember what you have planned. 
 
 This app design is a prototype for INF654 - Mobile Development I Fall Semester 2023 by Abigail Martinez
+
+This project was created from INF654 Lectures 1-9, but updated for customization
--- sw.js
+++ sw.js
@@ -15,10 +15,10 @@ const assets = [
 ];
 
 self.addEventListener("install", function (event) {
-  //fires when the browser install the app
-  //here we're just logging the event and the contents of the object passed to the event.
-  //the purpose of this event is to give the service worker a place to setup the local
-  //environment after the installation completes.
+/*fires when the browser install the app
+here we're just logging the event and the contents of the object passed to the event.
+the purpose of this event is to give the service worker a place to setup the local
+environment after the installation completes. */
   console.log(`SW: Event fired: ${event.type}`);
   event.waitUntil(
     caches.open(staticCache).then(function (cache) {
@@ -29,10 +29,10 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("activate", function (event) {
-  //fires after the service worker completes its installation.
-  // It's a place for the service worker to clean up from
-  // previous service worker versions.
-  // console.log(`SW: Event fired: ${event.type}`);
+/* fires after the service worker completes its installation.
+It's a place for the service worker to clean up from
+previous service worker versions.
+console.log(`SW: Event fired: ${event.type}`); */
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
@@ -43,11 +43,11 @@ self.addEventListener("activate", function (event) {
     })
   );
 });
-//comment
+
 self.addEventListener("fetch", function (event) {
-  //fires whenever the app requests a resource (file or data)
-  // console.log(`SW: Fetching ${event.request.url}`);
-  //next, go get the requested resource from the network
+/* fires whenever the app requests a resource (file or data)
+console.log(`SW: Fetching ${event.request.url}`);
+next, go get the requested resource from the network */
   event.respondWith(
     caches
       .match(event.request)
